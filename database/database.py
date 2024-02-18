e84a3a11a081ca3021d86dcf3913fa252330b7c6
wiizz <wiizz76.contact@gmail.com>
2024-02-18 22:16:09 +0100

    fix: environment variable

--- database/database.py
+++ database/database.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("postgres://bpump_user:AcCuL4lUWY9c5prDCnwn49Cymt3rjc4Q@dpg-cn971dacn0vc738qvtag-a.frankfurt-postgres.render.com/bpump")
+load_dotenv()
+
+engine = create_engine(os.environ.get("DB_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
