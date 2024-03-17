813e824611b50d699f324973266fc6846d59b457
wiizz <wiizz76.contact@gmail.com>
2024-03-17 12:31:28 +0100

    fix: using a brand new table for users programs

--- database/models.py
+++ database/models.py
@@ -1,13 +1,25 @@
 from sqlalchemy import Column, Integer, String, JSON
 from database.database import Base
 
-class User(Base):
-    __tablename__ = "user"
+class Users(Base):
+    __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True, nullable=False)
     username = Column(String, index=True, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    programs = Column(JSON, nullable=False)
+
+class Progs(Base):
+    __tablename__ = "progs"
+
+    prog_id = Column(Integer, primary_key=True, nullable=False)
+    owner = Column(String, index=True, nullable=False)
+    id = Column(String, index=True, nullable=False)
+    title = Column(String, index=True, nullable=False)
+    description = Column(String, index=True, nullable=False)
+    category = Column(String, index=True, nullable=False)
+    difficulty = Column(Integer, nullable=False)
+    hint = Column(JSON, nullable=False)
+    exercises = Column(JSON, nullable=False)
 
 class Exos(Base):
     __tablename__ = "exos"
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
-import json
 import bcrypt
 import database.models as models
 import database.schemas as schemas
@@ -28,32 +27,36 @@ def root():
 @app.post("/register")
 async def register(user_create: schemas.UserBase, db: db_dependency):
     try:
-        if db.query(models.User).filter(models.User.username == user_create.username).first():
+        if db.query(models.Users).filter(models.Users.username == user_create.username).first():
             raise HTTPException(status_code=400, detail="This user already exists")
         
         hashed_password = bcrypt.hashpw(user_create.password.encode("utf-8"), bcrypt.gensalt())
         default_programs = [ # The two default programs
-            {
-                "id": "cardio-intense",
-                "title": "Cardio Intense",
-                "description": "Un programme intense axé sur le renforcement cardiovasculaire.",
-                "category": "Cardio",
-                "difficulty": 4,
-                "hint": ["Restez hydraté pendant l'entraînement.", "Écoutez votre corps et ajustez l'intensité si nécessaire."],
-                "exercises": ["burpees", "jumpingjacks"],
-            },
-            {
-                "id": "renfo-corps",
-                "title": "Renfo du corps",
-                "description": "Un programme axé sur le renforcement des muscles du haut du corps.",
-                "category": "Haut du corps",
-                "difficulty": 3,
-                "hint": ["Assurez-vous de maintenir une bonne forme tout au long de l'exercice.", "Écoutez votre corps et ajustez l'intensité si nécessaire."],
-                "exercises": ["chinups", "dips"],
-            },
+            models.Progs(
+                owner=user_create.username,
+                id="cardio-intense",
+                title="Cardio Intense",
+                description="Un programme intense axé sur le renforcement cardiovasculaire.",
+                category="Cardio",
+                difficulty=4,
+                hint="Restez hydraté pendant l'entraînement. Écoutez votre corps et ajustez l'intensité si nécessaire.",
+                exercises="burpees, jumpingjacks"
+            ),
+            models.Progs(
+                owner=user_create.username,
+                id="renfo-corps",
+                title="Renfo du corps",
+                description="Un programme axé sur le renforcement des muscles du haut du corps.",
+                category="Haut du corps",
+                difficulty=3,
+                hint="Assurez-vous de maintenir une bonne forme tout au long de l'exercice. Écoutez votre corps et ajustez l'intensité si nécessaire.",
+                exercises="chinups, dips"
+            )
         ]
         
-        db_user = models.User(username=user_create.username, password=hashed_password.decode("utf-8"), programs=default_programs)
+        db_user = models.Users(username=user_create.username, password=hashed_password.decode("utf-8"))
+
+        db.add_all(default_programs)
 
         db.add(db_user)
         db.commit()
@@ -67,7 +70,7 @@ async def register(user_create: schemas.UserBase, db: db_dependency):
 @app.post("/login")
 async def login(user_create: schemas.UserBase, db: db_dependency):
     try:
-        user = db.query(models.User).filter(models.User.username == user_create.username).first()
+        user = db.query(models.Users).filter(models.Users.username == user_create.username).first()
 
         if user and bcrypt.checkpw(user_create.password.encode("utf-8"), user.password.encode("utf-8")):
             return {"status": True, "message": "User logged in successfully"}
@@ -75,33 +78,32 @@ async def login(user_create: schemas.UserBase, db: db_dependency):
             raise HTTPException(status_code=401, detail="Invalid credentials")
     except Exception as error:
         raise HTTPException(status_code=500, detail=f"Failed to log in. Error {str(error)}")
-
+    
 @app.post("/add_program")
 async def add_program(username: str, program: schemas.ProgramBase, db: db_dependency):
     try:
-        db_user = db.query(models.User).filter(models.User.username == username).first()
+        if db.query(models.Users).filter(models.Users.username == username).first():
+            new_program = models.Progs(
+                id=program.title.lower().replace(" ", "-"),
+                owner=username,
+                title=program.title,
+                description=program.description,
+                category=program.category,
+                difficulty=program.difficulty,
+                hint=program.hint,
+                exercises=program.exercises
+            )
 
-        if db_user:
-            program_dict = {
-                "id": program.title.lower().replace(" ", "-"),
-                "title": program.title,
-                "description": program.description,
-                "category": program.category,
-                "difficulty": program.difficulty,
-                "hint": program.hint,
-                "exercises": program.exercises
-            }
-
-            db_user.programs = db_user.programs + [program_dict]
+            db.add(new_program)
             db.commit()
-            
+
             return {"status": True, "message": "Program added successfully"}
         else:
             raise HTTPException(status_code=404, detail="User not found")
     except Exception as error:
         db.rollback()
         raise HTTPException(status_code=500, detail=f"Failed to add program. Error {str(error)}")
-    
+
 @app.post("/add_exercise")
 async def add_exercise(exercise: schemas.ExerciseBase, db: db_dependency):
     try:
@@ -115,29 +117,23 @@ async def add_exercise(exercise: schemas.ExerciseBase, db: db_dependency):
     except Exception as error:
         db.rollback()
         raise HTTPException(status_code=500, detail=f"Failed to add exercise. Error {str(error)}")
-    
+
 @app.get("/progs/{id}")
 async def read_program(id: str, username: str, db: db_dependency):
     try:
-        db_user = db.query(models.User).filter(models.User.username == username).first()
+        if not db.query(models.Users).filter(models.Users.username == username).first():
+            raise HTTPException(status_code=404, detail="User not found")
+
         if id == "all":
-            if db_user:
-                return db_user.programs or []
-            else:
-                raise HTTPException(status_code=404, detail="User not found")
+            return db.query(models.Progs).filter(models.Progs.owner == username).all()
         else:
-            if db_user and db_user.programs:
-                program = next((p for p in db_user.programs if p.get("id") == id), None)
-                
-                if program:
-                    return program
-                else:
-                    raise HTTPException(status_code=404, detail=f"Program with id '{id}' not found")
+            program = db.query(models.Progs).filter(models.Progs.owner == username, models.Progs.id == id).first()
+            if program:
+                return program
             else:
-                raise HTTPException(status_code=404, detail="User or programs not found")
+                raise HTTPException(status_code=404, detail="Program not found")
     except Exception as error:
         raise HTTPException(status_code=500, detail=f"Error while reading programs. Error {str(error)}")
-
 
 @app.get("/exos/{id}")
 async def read_exercise(id: str, db: db_dependency):
